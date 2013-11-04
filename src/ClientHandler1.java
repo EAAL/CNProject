4e3896722f6bf058d392825023f794ca2e318673
aryaz <aryaz@aryaz-UX31E.(none)>
2013-11-04 22:28:02 +0330

    database access used

--- src/ClientHandler1.java
+++ src/ClientHandler1.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -26,19 +27,37 @@ public class ClientHandler1 extends Thread{
 	public void run() {
 		
 		String fromClient = "";
+		//TODO get client's IP and put it inside IP
+		String IP = "127.0.0.1";
+		String userID = "";
+		String fileName = "";
+		String filePath = "";
+		String report = "";
 		System.out.println("client handling befor while");
 		outStream.println(" you are connected ");
 		while(fromClient!=null && (!fromClient.equals("disconnect"))){
 			System.out.println("******"+fromClient.split("\\s")[0]);
-			switch (fromClient.split("\\s")[0]) {
+			String command[] = fromClient.split("\\s");
+			switch (command[0]) {
 			case "Register":
 				System.out.println("man khafanam");
+				report = DataAccess.registerUser(command[1], IP);
 				break;
 			case "Save": //thread?
+				//TODO set userID, fileName and filePath
+				report = DataAccess.addFile(userID, fileName, filePath);
 				break;
 			case "Share":
+				//TODO set userID, fileName and filePath
+				report = DataAccess.addFile(userID, fileName, filePath);
 				break;
 			case "Update":
+				//TODO 
+				ArrayList<String> sharedWith = DataAccess.getUsersWhoHave(fileName);
+				for(String userName : sharedWith){
+					String userIP = DataAccess.getUserIPByID(userName);
+					//TODO send file to this IP
+				}
 				break;
 			default:
 				break;
